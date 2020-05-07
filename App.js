818aea3cfd15260ec4d673fc165c381029908c26
Max Magee <max.r.magee@gmail.com>
2020-05-06 21:03:28 -0500

    Added custom font and DefaultText component.

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React from "react";
+/* eslint-disable global-require */
+import React, { useState } from "react";
 import { enableScreens } from "react-native-screens";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 
 import productsReducer from "./store/reducers/products";
 import ShopNavigator from "./navigation/ShopNavigator";
@@ -14,7 +17,19 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />;
+  }
   return (
     <Provider store={store}>
       <ShopNavigator />
