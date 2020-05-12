7541515080e2af608035003cd6681ee35673ecb3
Max Magee <max.r.magee@gmail.com>
2020-05-12 16:37:01 -0500

    Products are now stored in firebase when added.

--- store/actions/products.js
+++ store/actions/products.js
@@ -3,9 +3,28 @@ export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 
 export const addProduct = (product) => {
-  return {
-    product,
-    type: ADD_PRODUCT,
+  const { title, imageUrl, price, description } = product;
+  return async (dispatch) => {
+    // any async code you want can go here...
+    const response = await fetch("https://rn-shop-de6c3.firebaseio.com/products.json", {
+      body: JSON.stringify({
+        description,
+        imageUrl,
+        price,
+        title,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+    const responseData = await response.json();
+
+    dispatch({
+      product: { ...product, id: responseData.name },
+      type: ADD_PRODUCT,
+    });
   };
 };
 
